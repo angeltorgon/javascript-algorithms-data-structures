9f448752429e202a57a8689487858191a78e76b7
angel-torres <alas4224@gmail.com>
2019-10-15 23:50:36 -0500

    added StringReversal challenge

---
+++ challenges/StringReversal.js
@@ -0,0 +1,21 @@
+const string = "abcdefghijklmnopqrstuvwxyz";
+
+// function reverseStr(arr) {
+//     return arr.split("").reduce((rev, character) => {
+//         return character + rev;
+//     }, "")
+// };
+
+// function reverseStr(arr) {
+//     let revArr = "";
+//     for(let character of arr) {
+//         revArr = character + rev
+//     }
+//     return revArr;
+// };
+
+// function reverseStr(arr) {
+//     return arr.split("").reverse().join("")
+// };
+
+console.log(reverseStr(string))
