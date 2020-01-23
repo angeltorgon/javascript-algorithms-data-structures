fd277773d1ab704549b718988b0a2a91aebf0fc4
angel-torres <alas4224@gmail.com>
2020-01-23 12:46:48 -0600

    added minimum-swaps-2.js

---
+++ hr/minimum-swaps-2.js
@@ -0,0 +1,35 @@
+/*
+
+You are given an unordered array consisting of consecutive integers without any duplicates. You are
+allowed to swap any two elements. You need to find the minimum number of swaps required to sort the
+array in ascending order. 
+
+e.g
+
+arr = [7,1,3,2,4,5,6]
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+
+returns 5
+
+*/
+
+
+function minimumSwaps(arr) {
+	console.log("arr", arr);
+
+}
+
+const arr1 = [1, 3, 2]; // 1
+const arr2 = [1, 4, 2, 3]; // 2
+const arr3 = [4, 1, 2, 3]; // 3
+
+minimumSwaps(arr1);
+
+
