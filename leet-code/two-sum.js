a7383513d4aa76be93be6ae4e1cf8cda446364fc
angel-torres <alas4224@gmail.com>
2020-01-24 12:01:28 -0600

    added documentation and a few tests

---
+++ leet-code/two-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * 
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 
+ * 
+ */
+
+var twoSum = function(nums, target) {
+	    
+};
+
+
+// -+^+-  -+^+-  -+^+-  -+^+-  -+^+-  TESTS
+
+const inputs1 = {nums: [1, 2, 3, 4], target:3};
+const inputs2 = {nums: [1, 2, 3, 4], target:4};
+const inputs3 = {nums: [1, 2, 3, 4], target:6};
+
+twoSum(inputs1.nums, inputs1.target);
+//twoSum(inputs2.nums, inputs2.target);
+//twoSum(inputs3.nums, inputs3.target);
