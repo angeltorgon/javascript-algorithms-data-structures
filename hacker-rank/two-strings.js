efa035398f1804f442dcae9315a888231eb4c320
angel-torres <alas4224@gmail.com>
2020-01-24 18:31:40 -0600

    created a few tests and worked out solution for two-strings.js

--- hacker-rank/two-strings.js
+++ hacker-rank/two-strings.js
@@ -1,7 +1,25 @@
 /*
+Given two strings, determine if they share a common substring. A substring may be as small as one character.
+
+For example, the words "a", "and", "art" share the common substring "a". The words "be" and "cat" do not share a substring.
+
+Substring can be as small as 1 char
 
 
+function twoStrings(s1,s2) { // s1 and s2 are strings
+	// should console.log "YES" if they share substring
+	// should console.log "NO" if they don't share substring
+}
 
+const string1 = "hello";
+const string2 = "he";
+
+twoStrings(string1, string2); // "Yes"
+
+
+// %-+-> %-+-> %-+-> %-+-> %-+-> %-+-> TESTS
+
+twoStrings("helo", "hello");
 
 */
 
@@ -9,16 +27,29 @@
 
 
 function twoStrings(s1,s2) {
-	console.log("s1", s1);
+	// initialize a map to store values of s1
+	const stringMap = {};
+
+	// loop over s1 to populate map
+	for(let character of s1){
+		stringMap[character] = stringMap[character] + 1 || 1;
+	};
+	
+	// loop over s2 to check if any value is in map
+	for(let character of s2) {
+		if(stringMap[character]) {
+			return "YES";
+		}
+	};
+	
+	return "NO";
 }
-
-
-
-
-
-
 
 
 // %-+-> %-+-> %-+-> %-+-> %-+-> %-+-> TESTS
 
-twoStrings("helo", "hello");
+
+console.log(twoStrings("km", "hello")); // NO
+console.log(twoStrings("he", "hello")); // YES
+console.log(twoStrings("how are you", "u")); // YES
+console.log(twoStrings("atopr", "mylkj")); // NO
