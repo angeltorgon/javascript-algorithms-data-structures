0aee08fc3b624d57f871f0729b1bf5970161388a
angel-torres <alas4224@gmail.com>
2020-01-24 18:04:02 -0600

    updated .gitignore so it ignores .swp files

---
+++ hacker-rank/two-strings.js
@@ -0,0 +1,24 @@
+/*
+
+
+
+
+*/
+
+// %-+-> %-+-> %-+-> %-+-> %-+-> %-+-> SOLUTION 1
+
+
+function twoStrings(s1,s2) {
+	console.log("s1", s1);
+}
+
+
+
+
+
+
+
+
+// %-+-> %-+-> %-+-> %-+-> %-+-> %-+-> TESTS
+
+twoStrings("helo", "hello");
