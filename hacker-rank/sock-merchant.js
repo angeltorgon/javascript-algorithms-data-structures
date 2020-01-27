fe85621b3d97ea8f382c067f38b1e7cdc7a50ad6
angel-torres <alas4224@gmail.com>
2020-01-27 11:07:09 -0600

    updated bubble-sort.js

---
+++ hacker-rank/sock-merchant.js
@@ -0,0 +1,27 @@
+/*
+
+Create a function that implements bubble sort. It is given an array and it must return 3 things. The
+number of swaps it took to sort the array, the first element of the sorted array, and the last
+element of the sorted array. 
+
+	Array is sorted in 0 swaps.
+	First Element: 1
+	Last Element: 3
+
+const numArr = [2,1,3,4];
+
+function countSwaps(a) {
+	
+	console.log(`Array is sorted in ${} swaps`);
+	console.log(`First Element: ${}`);
+	console.log(`Last Element: ${}`);
+
+}
+
+*/
+
+function countSwaps(a) {
+
+
+}
+
