bf5906533a94a6d941a18a985ca1ad8c616dd08b
angel-torres <alas4224@gmail.com>
2020-01-30 14:15:58 -0600

    finished solution for sherlock-anagrams

--- hacker-rank/sherlock-and-anagrams.js
+++ hacker-rank/sherlock-and-anagrams.js
@@ -2,7 +2,7 @@
 
 two strings are anagrams of each other if the letters of one string can be rearranged to form the other string. given a string, find the number of pairs of substrings of the string that are anagrams of each other.
 
-It must return an integer that represents the number of anagrammatic pairs of substrings in
+It must return an integer that represents the number of anagrammatic pairs of substrings 
 
 e.g.
 
@@ -14,19 +14,61 @@ function sherlockAndAnagrams(s) { // s = "abba"
 
 // $_=>> $_=>> $_=>> $_=>> $_=>> SOLUTION
 
-function sherlockandanagrams(s) {
-	console.log("s", s);
+function sherlockAndAnagrams(s) {
+	console.log("s -- ", s);
+	// initialize anagramMap
+	// initialize range
+	// initialize anagramCounter
+	const anagramMap = {};
+	let range = 1;
+	let anagramCounter = 0;
+
+	// while loop until range is === s.length
+	while(range < s.length){
+		// populate map using for loop
+		for(let i = 0; i < s.length; i++){
+			if(s[(i + range) - 1]){
+				const key = s.slice(i, i + range).split("").sort().join("");
+					//console.log("range", `${i} - ${i + range} : ${key}`);
+				anagramMap[key] = anagramMap[key] + 1 || 1;
+			};
+		};
+		range ++;
+	};
+	// loop over anagram and find all keys that have values of greater than 1
+	// increase counter if they are
+	for(let key in anagramMap) {
+		console.log("key: ", key);
+		const value = anagramMap[key];
+		if(value === 2) {
+			anagramCounter++;
+		}
+		if(value > 2) {
+			console.log("value", value);
+			for(let i = 0; i < value; i++) {
+				console.log(`i - ${i}....value - ${value}`);
+				for(let j = i + 1; j < value; j++) {
+					console.log("j---", j);	
+					anagramCounter ++;
+				}
+			};
+		}
+	};
+	return anagramCounter;
 }
 
 
 // $_=>> $_=>> $_=>> $_=>> $_=>> TESTS
 
-const s1 = "hellolskdlf";
-const s2 = "iwwiiwwi";
-const s3 "yuyuyyu";
+const s1 = "kkkk"; // 10 
+const s2 = "abba"; // 4
+const s3 = "yuyuyyu";
 const s4 = "osjfdlkksjf";
 
-console.log(sherlockAndAnagrams(s1));
+console.log("s1 --- ", sherlockAndAnagrams(s1));
+console.log("s2 --- ", sherlockAndAnagrams(s2));
+//console.log("s3 --- ", sherlockAndAnagrams(s3));
+//console.log("s4 --- ", sherlockAndAnagrams(s4));
 
 
 
