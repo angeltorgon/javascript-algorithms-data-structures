bf57441db62a6d83eb7c30025471c9144daad711
angel-torres <alas4224@gmail.com>
2020-01-31 22:19:56 -0600

    finished solution for diagonal-difference.js

--- hacker-rank/diagonal-difference.js
+++ hacker-rank/diagonal-difference.js
@@ -2,14 +2,22 @@
  *
  * DIAGONAL DIFFERENCE
  *
- *
- * Given an array of integers, find the sum of its elements.
+ * 
+ * Given a square matrix, calculate the absolute difference between 
+ * the sums of its diagonals.
  *
  * e.g.
  *
- * numArr = [1, 3, 4 ]
+ * Given matrix below.
  *
- * diagonalDifference(numArr) // returns 1 + 3 + 4
+ * matrix = [
+ * 				[1, 2, 3]
+ * 				[4, 5, 6]
+ * 				[9, 8, 9] 
+ * 			]
+ *
+ * diagonalDifference(matrix) // returns 2 -- > |(1+5+9) - (3+5+9)|
+ *                                                  14   -    17
  *
  *
  * */
@@ -20,8 +28,22 @@
  *
  * */
 
-function diagonalDifference(args) {
-	return null;
+function diagonalDifference(matrix) {
+	// initialize counter variable for left diagonal
+	let leftDiagonalCounter = 0;
+	// initialize counter variable for right diagonal
+	let rightDiagonalCounter = 0;
+	
+	// loop over matrix
+	for(let i = 0, j = matrix.length - 1; i < matrix.length; i++, j--) {
+		// each time we add a number to each of the counters
+		const leftValue = matrix[i][i];
+		const rightValue = matrix[i][j];
+		leftDiagonalCounter += leftValue;
+		rightDiagonalCounter += rightValue;
+	}
+    	
+	return Math.abs(leftDiagonalCounter - rightDiagonalCounter);
 };
 
 
@@ -31,13 +53,29 @@ function diagonalDifference(args) {
  *
  * */
 
-const numsArr1 = null;
-console.log("## TEST 1 ----- > ", diagonalDifference(numsArr1)); // 6
+const matrix1 = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[9, 8, 9]
+];
+console.log("## TEST 1 ----- > ", diagonalDifference(matrix1)); // 2
 
-const numsArr2 = null;
-console.log("## TEST 2 ----- > ", diagonalDifference(numsArr2)); // 100
+const matrix2 = [
+	[1, 2, 3, 4, 2],
+	[4, 5, 6, 4, 5],
+	[9, 8, 9, 2, 1],
+	[3, 0, 1, 2, 1],
+	[8, 4, 9, 2, 7]
+];
+console.log("## TEST 2 ----- > ", diagonalDifference(matrix2)); // 1
 
-const numsArr3 = null;
-console.log("## TEST 3 ----- > ", diagonalDifference(numsArr3)); // 128
-
+const matrix3 = [
+	[1, 2, 3, 4, 2, 4],
+	[4, 5, 6, 4, 5, 4],
+	[9, 8, 9, 2, 1, 4],
+	[3, 0, 1, 2, 1, 4],
+	[1, 6, 9, 2, 1, 4],
+	[8, 4, 9, 2, 7, 4]
+];
+console.log("## TEST 3 ----- > ", diagonalDifference(matrix3)); // 4
 
