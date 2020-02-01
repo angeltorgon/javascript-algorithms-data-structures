ecf019966b616731039dacf4543ae774fafc1e63
angel-torres <alas4224@gmail.com>
2020-01-31 23:55:50 -0600

    added staircase.js

---
+++ hacker-rank/staircase.js
@@ -0,0 +1,43 @@
+/*
+ *
+ * SIMPLE ARRAY SUM
+ *
+ *
+ * Given an array of integers, find the sum of its elements.
+ *
+ * e.g.
+ *
+ * numArr = [1, 3, 4 ]
+ *
+ * staircase(numArr) // returns 1 + 3 + 4
+ *
+ *
+ * */
+
+/*
+ *
+ * ***** SOLUTION
+ *
+ * */
+
+function staircase(args) {
+	return null;
+};
+
+
+
+/*
+ * ***** TESTS
+ *
+ * */
+
+const numsArr1 = null;
+console.log("## TEST 1 ----- > ", staircase(numsArr1)); // 6
+
+// const numsArr2 = null;
+// console.log("## TEST 2 ----- > ", staircase(numsArr2)); // 100
+
+// const numsArr3 = null;
+// console.log("## TEST 3 ----- > ", staircase(numsArr3)); // 128
+
+
