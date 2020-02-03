857b4790a912e36f29fdde80d38f9d27a6712693
angel-torres <alas4224@gmail.com>
2020-02-03 12:05:07 -0600

    added find-merge-node.js

---
+++ hacker-rank/find-merge-node.js
@@ -0,0 +1,45 @@
+/*
+ *
+ *  ------------------
+ * |  FIND MERGE NODE |
+ *  ------------------
+ *
+ *
+ * Given an array of integers, find the sum of its elements.
+ *
+ * e.g.
+ *
+ * numArr = [1, 3, 4 ]
+ *
+ * findMergeNode(numArr) // returns 1 + 3 + 4
+ *
+ *
+ * */
+
+/*
+ *
+ * ***** SOLUTION
+ *
+ * */
+
+function findMergeNode(args) {
+	return null;
+};
+
+
+
+/*
+ *
+ * ***** TESTS
+ *
+ * */
+
+const args1 = null;
+console.log("## TEST 1 ----- > ", findMergeNode(args1)); // 6
+
+// const args2 = null;
+// console.log("## TEST 2 ----- > ", findMergeNode(args2)); // 100
+
+// const args3 = null;
+// console.log("## TEST 3 ----- > ", findMergeNode(args3)); // 128
+
