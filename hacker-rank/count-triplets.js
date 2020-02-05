a35b0615cb87a374e7d3aae8b4faacc5b12ab188
angel-torres <alas4224@gmail.com>
2020-02-05 11:05:42 -0600

    updated documentation for count-triplets; working on solution

--- hacker-rank/count-triplets.js
+++ hacker-rank/count-triplets.js
@@ -5,13 +5,26 @@
  *  ----------------
  *
  *
- * Given an array of integers, find the sum of its elements.
+ * You are given an array of integers and `r`. Where `r`
+ * is a ratio. The `r` of 1 and 4 is 4, the `r` of 2 and 4 is 2, etc.
+ *
+ * You need to find three different indexes (i,j,k) where the values
+ * at those indexes all have `r` ratio and they are i<j<k.
+ *
+ * Find all the triplet indexes that meet these criteria.
+ *
+ * EDGE CASES
+ *  - duplicate values?
+ *      - count a triplet for each repeated value
+ *  - negatives?
  *
  * e.g.
  *
- * numArr = [1, 3, 4 ]
+ * numArr = [1, 3, 9, 9, 27, 81]
+ * r = 3
  *
- * countTriplets.js(numArr) // returns 1 + 3 + 4
+ * countTriplets.js(numArr, r) // returns 6
+ *
  *
  *
  * */
@@ -22,8 +35,8 @@
  *
  * */
 
-function countTriplets.js(args) {
-	return null;
+function countTriplets(numsArr, r) {
+	return numsArr;
 };
 
 
@@ -33,11 +46,12 @@ function countTriplets.js(args) {
  *
  * */
 
-const numsArr1 = null;
-console.log("## TEST 1 ----- > ", countTriplets.js(numsArr1)); // 6
+const numsArr1 = [1, 3, 9, 9, 27, 81]
+const r1 = 3
+console.log("## TEST 1 ----- > ", countTriplets(numsArr1, r1)); // 6
 
 // const numsArr2 = null;
-// console.log("## TEST 2 ----- > ", countTriplets.js(numsArr2)); // 100
+// console.log("## TEST 2 ----- > ", countTriplets(numsArr2)); // 100
 
 // const numsArr3 = null;
 // console.log("## TEST 3 ----- > ", countTriplets.js(numsArr3)); // 128
