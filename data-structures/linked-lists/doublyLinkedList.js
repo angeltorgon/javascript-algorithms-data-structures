955353156c9b34292c37d72f898e40eea4330260
angel-torres <alas4224@gmail.com>
2020-02-19 11:26:46 -0600

    added linkedListNode.js

--- data-structures/linked-lists/doublyLinkedList.js
+++ data-structures/linked-lists/doublyLinkedList.js
@@ -1,1 +1,18 @@
-console.log("DOUBLY LINKED LIST");
+class doublyLinkedList {
+    constructor() {
+        this.prev = null;
+        this.head = null;
+        this.next = null;
+    }
+
+    insert(data) {
+         
+    };
+}
+
+
+const doublyLinkedList1 = new doublyLinkedList();
+
+console.log("DOUBLY LINKED LIST", doublyLinkedList1.prev);
+console.log("DOUBLY LINKED LIST", doublyLinkedList1.prev);
+console.log("DOUBLY LINKED LIST", doublyLinkedList1.prev);
---
+++ data-structures/linked-lists/linkedListNode.js
@@ -0,0 +1,9 @@
+class linkedListNode {
+    constructor(data) {
+        self.data = data;
+        self.prev = null;
+        self.next = null;
+    }
+}
+
+module.exports = linkedListNode;
