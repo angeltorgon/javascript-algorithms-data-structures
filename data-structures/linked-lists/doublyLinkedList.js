97fe05139092d70e9d9ab5ac007d720065d32713
angel-torres <alas4224@gmail.com>
2020-02-20 00:33:47 -0600

    added insertAtTail to doublyLinkedList

--- data-structures/linked-lists/doublyLinkedList.js
+++ data-structures/linked-lists/doublyLinkedList.js
@@ -32,6 +32,20 @@ class doublyLinkedList {
             this.size ++;
         }
     };
+
+    insertAtTail(data) {
+        let node = new newNode(data);
+        if(this.size === 0) {
+            this.head = node;
+            this.tail = node;
+            this.size = 1;
+        } else {
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+            this.size ++;
+        }
+    };
 }
 
 /*
@@ -44,7 +58,20 @@ const doublyLinkedList1 = new doublyLinkedList();
 
 doublyLinkedList1.insertAtHead(1);
 doublyLinkedList1.insertAtHead(2);
+doublyLinkedList1.insertAtHead(3);
 
-console.log("HEAD - ", doublyLinkedList1.head);
+
+doublyLinkedList1.insertAtTail(4);
+doublyLinkedList1.insertAtTail(3);
+doublyLinkedList1.insertAtTail(2);
+
 console.log("SIZE - ", doublyLinkedList1.size);
+
+console.log("linked list contents");
+while(doublyLinkedList1.head !== null) {
+    console.log(doublyLinkedList1.head.data);
+    doublyLinkedList1.head = doublyLinkedList1.head.next;
+}
+
+console.log("tail - ", doublyLinkedList1.tail.data);
 
