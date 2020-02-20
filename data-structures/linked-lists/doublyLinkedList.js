c40440215df2137b950a2e5ddee11e08c640def8
angel-torres <alas4224@gmail.com>
2020-02-20 00:57:05 -0600

    added deleteAtHead method

--- data-structures/linked-lists/doublyLinkedList.js
+++ data-structures/linked-lists/doublyLinkedList.js
@@ -46,6 +46,16 @@ class doublyLinkedList {
             this.size ++;
         }
     };
+
+    deleteAtHead() {
+        if(this.size === 0) {
+            console.log("Linked list is empty.");
+        } else {
+            this.head.next.prev = null;
+            this.head = this.head.next;
+            this.size --;
+        }
+    };
 }
 
 /*
@@ -53,6 +63,22 @@ class doublyLinkedList {
  * Tests
  *
  * */
+
+function printLinkedList(list) {
+    console.log("SIZE - ", list.size);
+    console.log("head - ", doublyLinkedList1.head.data);
+    console.log("tail - ", doublyLinkedList1.tail.data);
+
+    const originalHead = list.head;
+
+    console.log("linked list contents");
+    while(list.head !== null) {
+        console.log(list.head.data);
+        list.head = list.head.next;
+    }
+
+    list.head = originalHead;
+}
 
 const doublyLinkedList1 = new doublyLinkedList();
 
@@ -65,13 +91,9 @@ doublyLinkedList1.insertAtTail(4);
 doublyLinkedList1.insertAtTail(3);
 doublyLinkedList1.insertAtTail(2);
 
-console.log("SIZE - ", doublyLinkedList1.size);
+printLinkedList(doublyLinkedList1)
 
-console.log("linked list contents");
-while(doublyLinkedList1.head !== null) {
-    console.log(doublyLinkedList1.head.data);
-    doublyLinkedList1.head = doublyLinkedList1.head.next;
-}
+doublyLinkedList1.deleteAtHead();
+doublyLinkedList1.deleteAtHead();
 
-console.log("tail - ", doublyLinkedList1.tail.data);
-
+printLinkedList(doublyLinkedList1)
